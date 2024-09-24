c28f2d1a8012177fbc66d12cf336b9d10bf3de29
Adriel117042 <144178518+Adriel117042@users.noreply.github.com>
2024-09-23 23:47:16 -0600

    Adriel Hernández Hernández PILA A,B,C
    Pila A se llena con letras mayúsculas, Pila B con Múltiplos de 3 y Pila C es la suma de las pilas A y B

--- Codigos/Pila.java
+++ Codigos/Pila.java
@@ -1,69 +1,160 @@
 import java.util.Scanner;
 
 public class Pila {
-    static int tope = 0;  // índice del tope de la pila
-    static int t = 0;     // tamaño de la pila
-    static int opt = 0;   // opción del menú
-    
+    static int t = 0;          // Tamaño de la pila
+    static int opcion = 0;     // Opción del menú
+    static int topeA = 0;      // Tope de la Pila A
+    static int topeB = 0;      // Tope de la Pila B
+    static int topeC = 0;      // Tope de la Pila C
+    static Object[] PilaC;     // Pila C para elementos alternados
+
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Tamaño de la pila:");
-            t = sc.nextInt();
-            
-            char[] pila = new char[t];  // Cambiamos a un arreglo de caracteres
-            
-            do {
-                System.out.println("1- Agregar\n"
-                                 + "2- Mostrar\n"
-                                 + "3- Eliminar\n"
-                                 + "4- Salir\n");
-                opt = sc.nextInt();
-                
-                switch(opt) {
-                    case 1: // Agregar letra a la pila
-                        if (tope < t) {
-                            System.out.println("Agrega una letra:");
-                            char letra = sc.next().charAt(0);
-                            if (Character.isLetter(letra)) {  // Verifica que sea una letra
-                                pila[tope] = letra;
-                                tope++;
-                            } else {
-                                System.out.println("Error: Solo se permiten letras");
-                            }
-                        } else {
-                            System.out.println("Error: Pila llena");
-                        }
-                        break;
-                        
-                    case 2: // Mostrar la pila
-                        if (tope > 0) {
-                            System.out.println("Elementos en la pila:");
-                            for (int i = tope - 1; i >= 0; i--) {
-                                System.out.println(pila[i]);
-                            }
-                        } else {
-                            System.out.println("La pila está vacía");
-                        }
-                        break;
-                        
-                    case 3: // Eliminar el último elemento
-                        if (tope > 0) {
-                            tope--;
-                            System.out.println("Elemento eliminado");
-                        } else {
-                            System.out.println("Error: Pila vacía");
-                        }
-                        break;
-                        
-                    case 4: // Salir
-                        System.out.println("Saliendo...");
-                        break;
-                        
-                    default:
-                        System.out.println("Opción no válida");
-                        break;
-                }
-            } while(opt != 4);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el tamaño de la pila: ");
+        t = sc.nextInt();
+        char[] PilaA = new char[t];  // Pila A para caracteres
+        int[] PilaB = new int[t];     // Pila B para enteros
+        PilaC = new Object[t * 2];    // Pila C para almacenar letras y enteros alternadamente
+
+        do {
+            System.out.println("1- Llenar Pila A\n"
+                             + "2- Llenar Pila B\n"
+                             + "3- Llenar Pila C\n"
+                             + "4- Mostrar\n"
+                             + "5- Salir del programa\n");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    llenarPilaA(PilaA);
+                    break;
+
+                case 2:
+                    llenarPilaB(PilaB);
+                    break;
+
+                case 3:
+                    llenarPilaC(PilaA, PilaB);
+                    break;
+
+                case 4://Sub Menu 
+                    System.out.println("a)- Mostrar Pila A\n" 
+                                     + "b)- Mostrar Pila B\n"
+                                     + "c)- Mostrar Pila C\n");
+
+                    char Opcion = sc.next().charAt(0);  
+                    switch (Opcion) {
+                        case 'a':
+                            mostrarPilaA(PilaA);
+                            break;
+                        case 'b':
+                            mostrarPilaB(PilaB);
+                            break;
+                        case 'c':
+                            mostrarPilaC();
+                            break;
+                        default:
+                            System.out.println("Opción no válida");
+                            break;
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Saliendo del programa...");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
+        } while (opcion != 5);
+
+        sc.close();
+    }
+
+    // Método para llenar Pila A con letras aleatorias
+    public static void llenarPilaA(char[] pila) {
+        for (int i = 0; i < pila.length; i++) {
+            pila[i] = (char) ('A' + (int)(Math.random() * 26));  // Genera letras aleatorias
+        }
+        topeA = pila.length;  // Actualizar el tope de la pila
+        System.out.println("Pila A llenada automáticamente con letras aleatorias");
+    }
+
+    // Método para llenar Pila B con múltiplos de 3
+    public static void llenarPilaB(int[] pila) {
+        int numero = 3;
+        for (int a = 0; a < pila.length; a++) {
+            pila[a] = numero;  // Llenar con múltiplos de 3
+            numero += 3;
+        }
+        topeB = pila.length;  // Actualizar el tope de la pila
+        System.out.println("Pila B llenada automáticamente con múltiplos de 3");
+    }
+
+    // Método para llenar Pila C con elementos alternados de A y B
+    public static void llenarPilaC(char[] pilaA, int[] pilaB) {
+        topeC = 0;  // Reiniciar el tope de Pila C
+
+        int minSize = Math.min(topeA, topeB);  // Determinar el tamaño mínimo de las pilas
+
+        // Llenar alternadamente
+        for (int i = 0; i < minSize; i++) {
+            PilaC[topeC++] = pilaA[i];  // Agregar elemento de Pila A
+            PilaC[topeC++] = pilaB[i];  // Agregar elemento de Pila B
+        }
+
+        // Si hay elementos sobrantes en Pila A
+        for (int i = minSize; i < topeA; i++) {
+            PilaC[topeC++] = pilaA[i];
+        }
+
+        // Si hay elementos sobrantes en Pila B
+        for (int i = minSize; i < topeB; i++) {
+            PilaC[topeC++] = pilaB[i];
+        }
+
+        System.out.println("Pila C llenada automáticamente con elementos alternados de Pila A y B");
+    }
+
+    // Método para mostrar Pila A
+    public static void mostrarPilaA(char[] pila) {
+        if (topeA > 0) {
+            System.out.println("Elementos en la Pila A:");
+            for (int i = 0; i < topeA; i++) {
+                System.out.println(pila[i]);
+            }
+        } else {
+            System.out.println("La Pila A está vacía");
+        }
+    }
+
+    // Método para mostrar Pila B
+    public static void mostrarPilaB(int[] pila) {
+        if (topeB > 0) {
+            System.out.println("Elementos en la Pila B:");
+            for (int i = 0; i < topeB; i++) {
+                System.out.println(pila[i]);
+            }
+        } else {
+            System.out.println("La Pila B está vacía");
+        }
+    }
+
+    // Método para mostrar Pila C
+    public static void mostrarPilaC() {
+        if (topeC > 0) {
+            System.out.println("Elementos en la Pila C:");
+            for (int i = 0; i < topeC; i++) {
+                System.out.println(PilaC[i]);
+            }
+        } else {
+            System.out.println("La Pila C está vacía");
         }
     }
 }
+
+
+
+
